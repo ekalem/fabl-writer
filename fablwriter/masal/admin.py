19319ad347b82f85360756de81c0e546f904c674
ekalem <ekalemdesign@gmail.com>
2018-07-29 19:09:11 +0300

    Add model: Fabl, Baglam, Sahne

---
+++ fablwriter/masal/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from masal.models import Fabl, Baglam, Sahne
+
+admin.site.register(Fabl)
+admin.site.register(Baglam)
+admin.site.register(Sahne)
+
+# Register your models here
