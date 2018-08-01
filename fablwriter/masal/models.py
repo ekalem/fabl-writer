def35b12d81b5c941e45d3b5e5e0203a18caf9ef
ekalem <ekalemdesign@gmail.com>
2018-08-01 15:05:08 +0300

    Hatalı olan kısımlar silinir

--- fablwriter/masal/models.py
+++ fablwriter/masal/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
-=======
 
 SAHNE_TIPLERI = (
     ('B', 'Başlık'),
@@ -16,7 +14,6 @@ SECENEK = (
     ('K', 'Kötü'),
 )
 
->>>>>>> f75797dee37a786e1f07ad5b389ab5cb546c2ef6
 class Fabl(models.Model):
     baslik = models.CharField(max_length=100)
 
@@ -26,29 +23,12 @@ class Fabl(models.Model):
 class Baglam(models.Model):
     anahtar = models.CharField(max_length=100)
     deger = models.CharField(max_length=100)
-<<<<<<< HEAD
-=======
     #fabl_id = models.ForeignKey(Fabl, on_delete=models.CASCADE)
->>>>>>> f75797dee37a786e1f07ad5b389ab5cb546c2ef6
 
     def __str__(self):
         return self.anahtar+" -- "+self.deger
 
 class Sahne(models.Model):
-<<<<<<< HEAD
-    SAHNE_TIPLERI = (
-        ('G', 'Girizgah'),
-        ('S', 'Serim'),
-        ('D', 'Düğüm'),
-        ('C', 'Çözüm'),
-        ('O', 'Öğüt'),
-    )
-    SECENEK = (
-        ('I', 'İyi'),
-        ('K', 'Kötü'),
-    )
-=======
->>>>>>> f75797dee37a786e1f07ad5b389ab5cb546c2ef6
     anahtar = models.CharField(max_length=1, choices=SAHNE_TIPLERI)
     deger = models.TextField()
     secenek = models.CharField(max_length=1, choices=SECENEK)
--- fablwriter/masal/views.py
+++ fablwriter/masal/views.py
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from masal.models import Baglam, Fabl
-from random import choice
-
-def home(request):
-    return render(request, 'home.html')
-
-def insanvehayvan(request):
-    karakter_ana = choice([i.deger for i in Baglam.objects.filter(anahtar='karakter_ana')])
-    karakter_yetkili = choice([i.deger for i in Baglam.objects.filter(anahtar='karakter_yetkili')])
-    karakter_hayvan = choice([i.deger for i in Baglam.objects.filter(anahtar='karakter_hayvan')])
-    karakter_kotu = choice([i.deger for i in Baglam.objects.filter(anahtar='karakter_kotu')])
-    zaman_belirteci = choice([i.deger for i in Baglam.objects.filter(anahtar='zaman_belirteci')])
-    mekan_acik_1 = choice([i.deger for i in Baglam.objects.filter(anahtar='mekan_acik')])
-    mekan_kapali_1 = choice([i.deger for i in Baglam.objects.filter(anahtar='mekan_kapali')])
-    mekan_kapali_2 = choice([i.deger for i in Baglam.objects.filter(anahtar='mekan_kapali')])
-    duygusal_tepki_1 = choice([i.deger for i in Baglam.objects.filter(anahtar='duygusal_tepki')])
-    duygusal_tepki_2 = choice([i.deger for i in Baglam.objects.filter(anahtar='duygusal_tepki')])
-    uzuv_1 = choice([i.deger for i in Baglam.objects.filter(anahtar='uzuv')])
-    uzuv_2 = choice([i.deger for i in Baglam.objects.filter(anahtar='uzuv')])
-    nesne_1 = choice([i.deger for i in Baglam.objects.filter(anahtar='nesne')])
-    kiyafet = choice([i.deger for i in Baglam.objects.filter(anahtar='kiyafet')])
-    sayi = choice(range(10))
-    return render(request, 'insanvehayvan.html', {
-        'karakter_ana': karakter_ana,
-        'karakter_yetkili': karakter_yetkili,
-        'karakter_hayvan': karakter_hayvan,
-        'karakter_kotu': karakter_kotu,
-        'zaman_belirteci': zaman_belirteci,
-        'mekan_acik_1': mekan_acik_1,
-        'mekan_kapali_1': mekan_kapali_1,
-        'mekan_kapali_2': mekan_kapali_2,
-        'duygusal_tepki_1': duygusal_tepki_1,
-        'duygusal_tepki_2': duygusal_tepki_2,
-        'uzuv_1': uzuv_1,
-        'uzuv_2': uzuv_2,
-        'nesne_1': nesne_1,
-        'kiyafet': kiyafet,
-        'sayi': sayi,
-=======
 from random import choice
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -100,5 +59,4 @@ def fabl_publish(request, id):
     return render(request, 'fabl_publish.html', {
         'title': id.baslik,
         'sahnes': sahnes,
->>>>>>> f75797dee37a786e1f07ad5b389ab5cb546c2ef6
     })
